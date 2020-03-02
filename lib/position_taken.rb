c512532cf6b4517f0cbd9ad3ffe4a0af42daae44
Josh Ellis <joshe523@gmail.com>
2020-03-02 03:13:02 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] == " " || board[index] == ""
-    return TRUE
-  end
     return FALSE
+  end
+    return TRUE
   end
